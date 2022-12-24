1c8edcd777c9bdd9e4bb3b714e1f3bdf88a4676b
armod <armods@gmail.com>
2022-12-24 12:01:16 +0100

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const url = 'https://api.gios.gov.pl/pjp-api/rest/station/findAll'
 // const url =
 //   'https://api.dane.gov.pl/1.4/resources/42634,liczba-studentow-z-obywatelstwem-ukrainskim-w-podziale-na-wojewodztwa_-stan-na-07112022/data'
 
+// potrzebny API_KEY https://openweathermap.org/price
 const api_call =
   'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
 
@@ -18,18 +19,17 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   const fetchData = async () => {
-setLoading(true)
-try {
-  const response = await fetch(url)
-  const data = await response.json()
-  setLoading(false)
-  setWeather(data)
-} catch (error) {
-  setLoading(false)
-  console.log('Error:', error)
-}
-
-    
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setLoading(false)
+      setStations(data)
+    } catch (error) {
+      setLoading(false)
+      console.log('Error:', error)
+    }
+  }
 
   useEffect(() => {
     fetchData()
