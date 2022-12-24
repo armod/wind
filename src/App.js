785c15a157a892621d9b4240989ed925a60343b7
armod <armods@gmail.com>
2022-12-24 11:59:19 +0100

    update

--- src/App.js
+++ src/App.js
@@ -10,57 +10,29 @@ const url = 'https://api.gios.gov.pl/pjp-api/rest/station/findAll'
 // const url =
 //   'https://api.dane.gov.pl/1.4/resources/42634,liczba-studentow-z-obywatelstwem-ukrainskim-w-podziale-na-wojewodztwa_-stan-na-07112022/data'
 
+const api_call =
+  'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+
 function App() {
   const [stations, setStations] = useState([])
   const [loading, setLoading] = useState(true)
 
-  const fetchStations = async () => {
-    fetch(url, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        //   'Content-Type': 'application/json',
-        // Host: 'api.gios.gov.pl',
-        // Accept: '*/*',
-        // Referer: 'http://localhost:3000/',
-        // Connection: 'keep-alive',
-        // 'Sec-Fetch-Mode': 'cors',
-        // 'access-control-allow-origin': 'powietrze.gios.gov.pl',
-        //   'Access-Control-Allow-Credentials': 'true',
-        // 'cache-control': 'max-age=120, public',
-        // 'cache-control': 'private',
-        // 'content-type': 'application/json;charset=utf-8',
-        // 'Access-Control-Allow-Origin': 'http://localhost:3000',
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      },
-    })
-      .then((response) => response.text('sa'))
-      .then((json) => console.log('dane:', json))
-      .catch((err) => console.log('Failed->', err.message))
-  }
-  //   access-control-allow-headers: Origin ,X-Requested-With ,Content-Type ,Accept ,Access-Control-Request-Method
-  // access-control-allow-methods: GET ,POST ,OPTIONS
-  // access-control-allow-origin: *
-  //   setLoading(true)
-  //   try {
-  //     const response = await fetch(url, {
-  //       // mode: 'cors',
-  //       // headers: { 'Access-Control-Allow-Origin': 'http://localhost:3000' },
-  //     })
-  //     console.log('data:', response)
-  //     const data = await response.json()
-  //     setLoading(false)
-  //     setStations(data)
-  //   } catch (error) {
-  //     setLoading(false)
-  //     // console.log('Error1: ', error.message)
-  //     console.log('Error2: ', error)
-  //   }
-  // }
+  const fetchData = async () => {
+setLoading(true)
+try {
+  const response = await fetch(url)
+  const data = await response.json()
+  setLoading(false)
+  setWeather(data)
+} catch (error) {
+  setLoading(false)
+  console.log('Error:', error)
+}
+
+    
 
   useEffect(() => {
-    fetchStations()
+    fetchData()
     console.log('effect:', stations)
   }, [])
 
