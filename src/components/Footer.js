6cd09221004c892f95c46a476d111e1d8f35603e
armod <armods@gmail.com>
2022-12-27 09:52:10 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,17 @@ import React from 'react'
 import styled from 'styled-components/macro'
 
 const Footer = () => {
-  return <Wrapper>Footer</Wrapper>
+  return (
+    <Wrapper>
+      <a href='https://www.weatherapi.com/' title='Free Weather API'>
+        <img
+          src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png'
+          alt='Weather data by WeatherAPI.com'
+          border='0'
+        />
+      </a>
+    </Wrapper>
+  )
 }
 
 export default Footer
