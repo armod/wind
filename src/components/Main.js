35d7deba80c3cab3c4640dcd1e20145f979fcad2
armod <armods@gmail.com>
2022-12-28 18:40:54 +0100

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,19 @@ import styled from 'styled-components/macro'
 
 const Main = ({ stations }) => {
   // console.log('main-log', stations)
+  const imgName = stations.current.condition.icon.substring(
+    stations.current.condition.icon.length - 7
+  )
   return (
     <Wrapper>
       <h2>{stations.location.name}</h2>
+      <h2>{stations.current.temp_c}&deg;C</h2>
+      <img src={`../assetss/images/nigth/${imgName}`} alt='icon' />
+      <img src='../assetss/images/night/119.png' alt='icon' />
+      <h4>{stations.current.condition.text}</h4>
+      <h4>prędkość wiatru{stations.current.wind_kph}m/s</h4>
+      <h4>ciśnienie{stations.current.pressure_mb}hPa</h4>
+      <h4>opady{stations.current.humidity}%</h4>
     </Wrapper>
   )
 }
